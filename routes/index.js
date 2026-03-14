1ddd64d6086a10169a168844752fccb1120ac6c7
Bridger Fronk <bridgerfronk@gmail.com>
2026-03-14 16:51:53 -0400

    init

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello Wjknfxzhgnclvbkjojfkebhvcalervsgbhljvbsjlkdfghnmwmbsafdsjkhbvxzjnhb;r /dksxjcdaclhjdgbv.mehSRBGAV.HKNMfxnb;ositdklzhjbrld!');
+});
+
+module.exports = router;
